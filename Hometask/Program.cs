674d987a138801011cf1d57cd357d752e1e45e47
Tural003 <turaleliyev702@gmail.com>
2023-09-28 17:13:57 +0400

    new task :all task code

---
+++ Hometask/Program.cs
@@ -0,0 +1,86 @@
+﻿//-------------------------------------------------------Array yartamaq--------------------------------------------
+
+
+//Console.WriteLine("Eddeleri daxil edin  :");
+//int[]array=new int[5];
+//;    
+//for ( int i = 0; i < array.Length; i++)
+//{
+//    array[i] = int.Parse(Console.ReadLine());
+//}
+//for (int i = 0; i <array.Length; i++)
+//{
+//    Console.Write(  array[i]);    
+//}
+
+//----------------------------------------------------------------Task1---------------------------------------------------------
+
+//Console.WriteLine("Arrayin elementlerini daxil edin :");
+//int [] arr = new int[4];
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    arr[i] = int.Parse(Console.ReadLine());
+//}
+//int sum = 0;
+
+//for (int i = 0; i <arr.Length; i++)
+//{
+//    sum = sum + arr[i];
+//}
+//Console.WriteLine("Arrayin elementleri cemi : "+ sum);
+
+
+//-------------------------------------------------------------------Task2-----------------------------------------------------
+//Console.WriteLine("Arraye elementleri daxil edin :");
+
+//string[] array= new string[5];
+
+//for (int i = 0; i <array.Length; i++)
+//{
+//    array[i] = Console.ReadLine();
+//}
+
+
+//Console.WriteLine("Herflerin ayi 4 den cox olan adlar:");
+//for (int i = 0; i <array.Length; i++)
+//{
+//    if (array[i].Length > 4)
+//    {
+//        Console.WriteLine(array[i]);
+//    }
+
+//}
+
+//-------------------------------------------------------------------Task3-----------------------------------------------------
+
+
+
+//Console.WriteLine("Eddeleri daxil edin  :");
+//int[]array=new int[5];
+
+//for (int i = 0; i < array.Length; i++)
+//{
+//    array[i] = int.Parse(Console.ReadLine());
+//}
+
+//int temp;
+//for (int i = 0; i < array.Length; i++)
+//{
+//    for (int a = i; a < array.Length; a++)
+//    {
+//        if (array[i] < array[a])
+//        {
+//            temp = array[i];
+//            array[i] = array[a];
+//            array[a] = temp;
+//        }
+
+//    }
+
+
+//}
+//for (int i = 0;  i <array.Length; i++)
+//{
+//    Console.WriteLine(array[i]);
+//}
+
